da9b2ebfce680d03b6cdc09ed7ad364ea7bbe5a6
Isaac Ferreira <zaclummys@gmail.com>
2025-05-25 23:39:35 +0000

    Add script to download data

---
+++ download-data.sh
@@ -0,0 +1,9 @@
+set -x
+cd "$(dirname "$0")"
+
+wget https://osf.io/download/gpmvy/ -O ./data/crime.csv
+wget https://osf.io/download/bxt6n/ -O ./data/district.csv
+wget https://osf.io/download/vrmy5/ -O ./data/neighborhood.csv
+wget https://osf.io/download/67dqy/ -O ./data/segment.csv
+wget https://osf.io/download/5hrc6/ -O ./data/time.csv
+wget https://osf.io/download/u7jgq/ -O ./data/vertice.csv
