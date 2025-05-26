448a074350e0cde70e97395cfc6096e4996dcb3f
Isaac Ferreira <zaclummys@gmail.com>
2025-05-26 00:07:32 +0000

    Add script to copy schema to hive server

---
+++ copy-schema-to-hive-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+cd "$(dirname "$0")"
+
+docker cp schema/drop-tables.sql hive-hive-server-1:/tmp/schema
+docker cp schema/create-tables.sql hive-hive-server-1:/tmp/schema
+
+docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/drop-tables.sql
+docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/create-tables.sql
