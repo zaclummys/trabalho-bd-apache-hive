c156b2dc65002130f1429fabe855b1ed148de699
Isaac Ferreira <zaclummys@gmail.com>
2025-05-26 01:08:00 +0000

    Update copy data to hive server script

--- copy-data-to-hive-server.sh
+++ copy-data-to-hive-server.sh
@@ -3,9 +3,23 @@
 set -x
 cd "$(dirname "$0")"
 
-docker cp data/crime.csv hive-hive-server-1:/tmp/
-docker cp data/segment.csv hive-hive-server-1:/tmp/
-docker cp data/vertice.csv hive-hive-server-1:/tmp/
-docker cp data/time.csv hive-hive-server-1:/tmp/
-docker cp data/district.csv hive-hive-server-1:/tmp/
-docker cp data/neighborhood.csv hive-hive-server-1:/tmp/
+data=(
+    "crime"
+    "segment"
+    "vertice"
+    "time"
+    "district"
+    "neighborhood"
+)
+
+for file in "${data[@]}"; do
+    docker cp data/$file.csv hive-hive-server-1:/tmp/
+done
+
+for table in "${data[@]}"; do
+    docker exec -it hive-hive-server-1 bash -c "beeline -u jdbc:hive2://localhost:10000 -e \"LOAD DATA LOCAL INPATH '/tmp/$table.csv' INTO TABLE $table;\""
+done
+
+for table in "${data[@]}"; do
+    docker exec -it hive-hive-server-1 bash -c "beeline -u jdbc:hive2://localhost:10000 -e \"SELECT COUNT(*) as $table FROM $table;\""
+done
