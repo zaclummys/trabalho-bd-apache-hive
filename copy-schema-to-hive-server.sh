46b872d1d74db4938f25b2d510e1cce4ca723654
Isaac Ferreira <zaclummys@gmail.com>
2025-05-26 01:21:14 +0000

    Copy whole schema folder

--- copy-schema-to-hive-server.sh
+++ copy-schema-to-hive-server.sh
@@ -3,8 +3,7 @@
 set -x
 cd "$(dirname "$0")"
 
-docker cp schema/drop-tables.sql hive-hive-server-1:/tmp
-docker cp schema/create-tables.sql hive-hive-server-1:/tmp
+docker cp schema/. hive-hive-server-1:/tmp/schema/
 
-docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/drop-tables.sql
-docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/create-tables.sql
+docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/drop-tables.sql
+docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/create-tables.sql
