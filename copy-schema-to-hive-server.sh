bc7c42142f61dc0bf75f2e3c25252bdb3ee0956a
Isaac Ferreira <zaclummys@gmail.com>
2025-05-26 01:08:22 +0000

    Remove schema as target folder

--- copy-schema-to-hive-server.sh
+++ copy-schema-to-hive-server.sh
@@ -3,8 +3,8 @@
 set -x
 cd "$(dirname "$0")"
 
-docker cp schema/drop-tables.sql hive-hive-server-1:/tmp/schema
-docker cp schema/create-tables.sql hive-hive-server-1:/tmp/schema
+docker cp schema/drop-tables.sql hive-hive-server-1:/tmp
+docker cp schema/create-tables.sql hive-hive-server-1:/tmp
 
-docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/drop-tables.sql
-docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/create-tables.sql
+docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/drop-tables.sql
+docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/create-tables.sql
