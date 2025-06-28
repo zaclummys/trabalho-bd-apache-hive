5d90481faf8e56def0099adedbab445b514f5443
Isaac Ferreira <zaclummys@gmail.com>
2025-06-28 19:08:14 +0000

    drop schema before creating it

--- copy-schema-to-hive-server.sh
+++ copy-schema-to-hive-server.sh
@@ -5,4 +5,5 @@ cd "$(dirname "$0")"
 
 docker cp schema/. hive-hive-server-1:/tmp/schema/
 
+docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/drop-tables.sql
 docker exec hive-hive-server-1 beeline -u jdbc:hive2://localhost:10000 -f /tmp/schema/create-tables.sql
